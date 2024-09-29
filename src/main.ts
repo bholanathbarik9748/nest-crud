10890b80706877010368c17f26aecdbf9a357193
Bholanath <bholanathbarik9748@gmail.com>
2024-09-29 11:48:31 +0530

    Feat: add morgan logger to see logs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
 
 async function mainApp() {
   const app = await NestFactory.create(AppModule);
+  app.use(morgan('dev'));
   app.setGlobalPrefix('api/v1');
   await app.listen(3000);
 }
